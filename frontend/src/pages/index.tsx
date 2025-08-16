884182f0d4e84809ee9fcdc8333ba2a89bb4ad89
Laurence Cortez <laurencecortez26.lc@gmail.com>
2025-08-16 13:22:31 +0800

    test material ui

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -3,12 +3,15 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
 
 
 export default function Home() {
   return (
     <>
-      <Button variant="contained">Click Me!</Button>
+      <Box component="div" sx={{ p: 2 }}>
+        <Button variant="contained">Click Me!</Button>
+      </Box>
     </>
   );
 }
