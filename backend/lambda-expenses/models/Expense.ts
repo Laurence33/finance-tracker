2d52f5587b28890b05627d824534d52ae1b22e69
Laurence Cortez <laurencecortez26.lc@gmail.com>
2025-09-21 20:08:21 +0800

    refactor Expense model

--- backend/lambda-expenses/models/Expense.ts
+++ backend/lambda-expenses/models/Expense.ts
@@ -1,39 +1,32 @@
-/* eslint-disable prettier/prettier */
 import { CreateExpenseRequestBody } from '../types/Expense';
 export const EXPENSE_PK = 'Expense#Expense';
 export class Expense {
-  private PK = EXPENSE_PK;
-  private timestamp: string;
-  private fundSource: string;
-  private amount: number;
+    private PK = EXPENSE_PK;
+    private timestamp: string;
+    private fundSource: string;
+    private amount: number;
 
-  constructor(body: CreateExpenseRequestBody | Record<string, any>, fromDdb = false) {
-    if (!fromDdb) {
-      this.timestamp = body.timestamp.replace('T', ' ');
-      this.fundSource = body.fundSource;
-      this.amount = body.amount;
+    constructor(data: CreateExpenseRequestBody | Record<string, any>) {
+        if (data.SK) this.timestamp = data.SK; // from DynamoDB
+        else this.timestamp = data.timestamp.replace('T', ' ');
+        this.fundSource = data.fundSource;
+        this.amount = data.amount;
     }
-    else {
-      this.timestamp = body.SK;
-      this.fundSource = body.fundSource;
-      this.amount = body.amount;
+
+    toDdbItem() {
+        return {
+            PK: this.PK,
+            SK: this.timestamp,
+            fundSource: this.fundSource,
+            amount: this.amount,
+        };
     }
-  }
 
-  toDdbItem() {
-    return {
-      PK: this.PK,
-      SK: this.timestamp,
-      fundSource: this.fundSource,
-      amount: this.amount,
-    };
-  }
-
-  toNormalItem() {
-    return {
-      timestamp: this.timestamp,
-      fundSource: this.fundSource,
-      amount: this.amount,
-    };
-  }
+    toNormalItem() {
+        return {
+            timestamp: this.timestamp,
+            fundSource: this.fundSource,
+            amount: this.amount,
+        };
+    }
 }
--- backend/lambda-expenses/services/ExpensesService.ts
+++ backend/lambda-expenses/services/ExpensesService.ts
@@ -9,139 +9,130 @@ import { ddbDocClient } from './ddb-client';
 
 const SINGLE_TABLE_NAME = process.env.DDB_TABLE_NAME || 'SingleTable';
 export class ExpensesService {
-  async createExpense(body: CreateExpenseRequestBody) {
+    async createExpense(body: CreateExpenseRequestBody) {
+        const validationResult = CreateExpenseValidator.safeParse(body);
+        if (!validationResult.success) {
+            const errors = treeifyError(validationResult.error).properties;
+            return createBadRequestResponse(HttpStatus.BAD_REQUEST, 'Validation failed', errors);
+        }
 
-    const validationResult = CreateExpenseValidator.safeParse(body);
-    if (!validationResult.success) {
-      const errors = treeifyError(validationResult.error).properties;
-      return createBadRequestResponse(
-        HttpStatus.BAD_REQUEST,
-        'Validation failed',
-        errors
-      );
+        const checkExpense = await getExpense(validationResult.data.timestamp);
+        if (checkExpense) {
+            return createBadRequestResponse(
+                HttpStatus.BAD_REQUEST,
+                'An expense with the same timestamp already exists.',
+            );
+        }
+
+        const expense = new Expense(validationResult.data);
+        const command = new PutCommand({
+            TableName: SINGLE_TABLE_NAME,
+            Item: expense.toDdbItem(),
+        });
+
+        await ddbDocClient.send(command);
+
+        return createSuccessResponse(HttpStatus.OK, {
+            message: 'Expense recorded successfully',
+            data: expense.toNormalItem(),
+        });
     }
 
-    const checkExpense = await getExpense(validationResult.data.timestamp);
-    if (checkExpense) {
-      return createBadRequestResponse(HttpStatus.BAD_REQUEST, 'An expense with the same timestamp already exists.');
+    async getExpenses() {
+        const command = new QueryCommand({
+            TableName: SINGLE_TABLE_NAME,
+            KeyConditionExpression: 'PK = :pk',
+            ExpressionAttributeValues: {
+                ':pk': EXPENSE_PK,
+            },
+        });
+        const response = await ddbDocClient.send(command);
+        return createSuccessResponse(HttpStatus.OK, {
+            message: 'Expenses retrieved successfully',
+            data: response.Items?.map((item) => new Expense(item).toNormalItem()),
+        });
     }
 
-    const expense = new Expense(validationResult.data);
-    const command = new PutCommand({
-      TableName: SINGLE_TABLE_NAME,
-      Item: expense.toDdbItem(),
-    });
+    async deleteExpense(timestamp?: string) {
+        if (!isValidDate(timestamp || '')) {
+            return createBadRequestResponse(HttpStatus.BAD_REQUEST, 'Invalid or missing timestamp');
+        }
 
-    await ddbDocClient.send(command);
-
-    return createSuccessResponse(HttpStatus.OK, {
-      message: 'Expense recorded successfully',
-      data: expense.toNormalItem(),
-    });
-  }
-
-  async getExpenses() {
-    const command = new QueryCommand({
-      TableName: SINGLE_TABLE_NAME,
-      KeyConditionExpression: 'PK = :pk',
-      ExpressionAttributeValues: {
-        ':pk': EXPENSE_PK,
-      },
-    });
-    const response = await ddbDocClient.send(command);
-    return createSuccessResponse(HttpStatus.OK, {
-      message: 'Expenses retrieved successfully',
-      data: response.Items?.map((item) => new Expense(item, true).toNormalItem()),
-    });
-  }
-
-  async deleteExpense(timestamp?: string) {
-    if (!isValidDate(timestamp || '')) {
-      return createBadRequestResponse(HttpStatus.BAD_REQUEST, 'Invalid or missing timestamp');
+        await deleteExpenseItem(timestamp || '');
+        return createSuccessResponse(HttpStatus.NO_CONTENT);
     }
 
-    await deleteExpenseItem(timestamp || '');
-    return createSuccessResponse(HttpStatus.NO_CONTENT);
-  }
+    async updateExpense(timestamp: string, body: Partial<CreateExpenseRequestBody>) {
+        if (!isValidDate(timestamp)) {
+            return createBadRequestResponse(HttpStatus.BAD_REQUEST, 'Invalid or missing timestamp');
+        }
 
-  async updateExpense(timestamp: string, body: Partial<CreateExpenseRequestBody>) {
-    if (!isValidDate(timestamp)) {
-      return createBadRequestResponse(HttpStatus.BAD_REQUEST, 'Invalid or missing timestamp');
+        const validationResult = CreateExpenseValidator.partial().safeParse(body);
+        if (!validationResult.success) {
+            const errors = treeifyError(validationResult.error).properties;
+            return createBadRequestResponse(HttpStatus.BAD_REQUEST, 'Validation failed', errors);
+        }
+
+        // Fetch existing expense
+        const checkExpense = await getExpense(timestamp);
+        if (!checkExpense) {
+            return createBadRequestResponse(HttpStatus.NOT_FOUND, 'Expense not found');
+        }
+
+        const existingExpense = checkExpense.toNormalItem();
+
+        // If the timestamp is changed in the update, we need to delete the old item
+        if (validationResult.data.timestamp && validationResult.data.timestamp !== existingExpense.timestamp) {
+            // need to check if the new timestamp already exists
+            const checkNewExpense = await getExpense(validationResult.data.timestamp);
+            if (checkNewExpense) {
+                return createBadRequestResponse(
+                    HttpStatus.BAD_REQUEST,
+                    'An expense with the new timestamp already exists.',
+                );
+            }
+
+            await deleteExpenseItem(existingExpense.timestamp);
+        }
+
+        const updatedData = { ...existingExpense, ...validationResult.data };
+        const updatedExpense = new Expense(updatedData);
+        const putCmd = new PutCommand({
+            TableName: SINGLE_TABLE_NAME,
+            Item: updatedExpense.toDdbItem(),
+        });
+        await ddbDocClient.send(putCmd);
+
+        return createSuccessResponse(HttpStatus.OK, {
+            message: 'Expense updated successfully',
+            data: updatedExpense.toNormalItem(),
+        });
     }
-
-    const validationResult = CreateExpenseValidator.partial().safeParse(body);
-    if (!validationResult.success) {
-      const errors = treeifyError(validationResult.error).properties;
-      return createBadRequestResponse(
-        HttpStatus.BAD_REQUEST,
-        'Validation failed',
-        errors
-      );
-    }
-
-    // Fetch existing expense
-    const checkExpense = await getExpense(timestamp);
-    if (!checkExpense) {
-      return createBadRequestResponse(HttpStatus.NOT_FOUND, 'Expense not found');
-    }
-
-    const existingExpense = checkExpense.toNormalItem();
-
-    // If the timestamp is changed in the update, we need to delete the old item
-    if (
-      validationResult.data.timestamp &&
-      validationResult.data.timestamp !== existingExpense.timestamp
-    ) {
-      // need to check if the new timestamp already exists
-      const checkNewExpense = await getExpense(validationResult.data.timestamp);
-      if (checkNewExpense) {
-        return createBadRequestResponse(HttpStatus.BAD_REQUEST, 'An expense with the new timestamp already exists.');
-      }
-
-      await deleteExpenseItem(existingExpense.timestamp);
-    }
-
-    const updatedData = { ...existingExpense, ...validationResult.data };
-    const updatedExpense = new Expense(updatedData);
-    const putCmd = new PutCommand({
-      TableName: SINGLE_TABLE_NAME,
-      Item: updatedExpense.toDdbItem(),
-    });
-    await ddbDocClient.send(putCmd);
-
-    return createSuccessResponse(HttpStatus.OK, {
-      message: 'Expense updated successfully',
-      data: updatedExpense.toNormalItem(),
-    });
-  }
-
-
-
 }
 
 async function deleteExpenseItem(timestamp: string) {
-  const deleteCmd = new DeleteCommand({
-    TableName: SINGLE_TABLE_NAME,
-    Key: {
-      PK: EXPENSE_PK,
-      SK: timestamp,
-    },
-  });
-  await ddbDocClient.send(deleteCmd);
+    const deleteCmd = new DeleteCommand({
+        TableName: SINGLE_TABLE_NAME,
+        Key: {
+            PK: EXPENSE_PK,
+            SK: timestamp,
+        },
+    });
+    await ddbDocClient.send(deleteCmd);
 }
 
 async function getExpense(timestamp: string): Promise<Expense | null> {
-  const command = new QueryCommand({
-    TableName: SINGLE_TABLE_NAME,
-    KeyConditionExpression: 'PK = :pk AND SK = :sk',
-    ExpressionAttributeValues: {
-      ':pk': EXPENSE_PK,
-      ':sk': timestamp,
-    },
-  });
-  const response = await ddbDocClient.send(command);
-  if (!response.Items || response.Items.length === 0) {
-    return null;
-  }
-  return new Expense(response.Items[0], true);
+    const command = new QueryCommand({
+        TableName: SINGLE_TABLE_NAME,
+        KeyConditionExpression: 'PK = :pk AND SK = :sk',
+        ExpressionAttributeValues: {
+            ':pk': EXPENSE_PK,
+            ':sk': timestamp,
+        },
+    });
+    const response = await ddbDocClient.send(command);
+    if (!response.Items || response.Items.length === 0) {
+        return null;
+    }
+    return new Expense(response.Items[0]);
 }
