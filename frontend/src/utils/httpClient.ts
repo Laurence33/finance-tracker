0db870b4fe94ab973060de42faea775749ce5260
Laurence Cortez <laurencecortez26.lc@gmail.com>
2025-09-22 12:54:22 +0800

    remove logs

--- frontend/src/utils/httpClient.ts
+++ frontend/src/utils/httpClient.ts
@@ -15,11 +15,9 @@ function handleHttpException(error: any): void {
     // The request was made but no response was received
     // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
     // http.ClientRequest in node.js
-    console.log(error.request);
     throw new Error('No response received from server.');
   } else {
     // Something happened in setting up the request that triggered an Error
-    console.log('Error', error.message);
     throw new Error(error.message);
   }
 }
