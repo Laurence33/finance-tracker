b00f39fc5bb617d6a3327af348f6be3b7164fc01
Laurence Cortez <laurencecortez26.lc@gmail.com>
2025-09-22 12:52:16 +0800

    http error handling on frontend

--- frontend/src/utils/httpClient.ts
+++ frontend/src/utils/httpClient.ts
@@ -6,24 +6,58 @@ const httpClient = axios.create({
   },
 });
 
+function handleHttpException(error: any): void {
+  if (error.response) {
+    // The request was made and the server responded with a status code
+    // that falls out of the range of 2xx
+    throw new Error(error.response.data.message || 'Unknown error occurred.');
+  } else if (error.request) {
+    // The request was made but no response was received
+    // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+    // http.ClientRequest in node.js
+    console.log(error.request);
+    throw new Error('No response received from server.');
+  } else {
+    // Something happened in setting up the request that triggered an Error
+    console.log('Error', error.message);
+    throw new Error(error.message);
+  }
+}
+
 export class HttpClient {
-  static async get<T>(url: string): Promise<T> {
-    const response = await httpClient.get<T>(url);
-    return response.data;
+  static async get<T>(url: string): Promise<T | void> {
+    try {
+      const response = await httpClient.get<T>(url);
+      return response.data;
+    } catch (error: any) {
+      return handleHttpException(error);
+    }
   }
 
-  static async post<T>(url: string, data: any): Promise<T> {
-    const response = await httpClient.post<T>(url, data);
-    return response.data;
+  static async post<T>(url: string, data: any): Promise<T | void> {
+    try {
+      const response = await httpClient.post<T>(url, data);
+      return response.data;
+    } catch (error: any) {
+      return handleHttpException(error);
+    }
   }
 
-  static async put<T>(url: string, data: any): Promise<T> {
-    const response = await httpClient.put<T>(url, data);
-    return response.data;
+  static async put<T>(url: string, data: any): Promise<T | void> {
+    try {
+      const response = await httpClient.put<T>(url, data);
+      return response.data;
+    } catch (error: any) {
+      return handleHttpException(error);
+    }
   }
 
-  static async delete<T>(url: string): Promise<T> {
-    const response = await httpClient.delete<T>(url);
-    return response.data;
+  static async delete<T>(url: string): Promise<T | void> {
+    try {
+      const response = await httpClient.delete<T>(url);
+      return response.data;
+    } catch (error: any) {
+      return handleHttpException(error);
+    }
   }
 }
