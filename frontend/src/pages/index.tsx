858506d9e8a3ceda62d9381a8d2ef77111b44587
Laurence Cortez <laurencecortez26.lc@gmail.com>
2025-09-28 11:01:01 +0800

    cleanup

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,8 +1,6 @@
 import Box from '@mui/material/Box';
-import React, { useState, useEffect, use } from 'react';
-import { HttpClient } from '../utils/httpClient';
+import React, { useEffect, use } from 'react';
 import CreateExpenseForm from '@/components/molecules/CreateExpenseForm';
-import { Expense } from '@/types/Expense';
 import ExpenseList from '@/components/molecules/ExpensesList';
 import { AppContext } from '@/context/AppContext';
 
