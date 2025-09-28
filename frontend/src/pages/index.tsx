90bc6e4bedd0e97d0b44495e4d0ad4d0c565bc4a
Laurence Cortez <laurencecortez26.lc@gmail.com>
2025-09-28 10:22:24 +0800

    cleanup

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,23 +1,14 @@
 import Box from '@mui/material/Box';
-import Stack from '@mui/material/Stack';
-import Snackbar from '@mui/material/Snackbar';
-import Alert from '@mui/material/Alert';
 import React, { useState, useEffect, use } from 'react';
 import { HttpClient } from '../utils/httpClient';
 import CreateExpenseForm from '@/components/molecules/CreateExpenseForm';
 import { Expense } from '@/types/Expense';
 import ExpenseList from '@/components/molecules/ExpensesList';
-import { SnackBarState } from '@/types/SnackBarState';
 import { AppContext } from '@/context/AppContext';
 
 export default function Home() {
   const [expenses, setExpenses] = useState<Expense[]>([]);
-  const {
-    snackBarState,
-    showErrorSnackBar,
-    showSuccessSnackBar,
-    handleSnackBarClose,
-  } = use(AppContext);
+  const { showErrorSnackBar, showSuccessSnackBar } = use(AppContext);
 
   useEffect(() => {
     fetchExpenses();
