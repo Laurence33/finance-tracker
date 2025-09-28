d66135a3cfcfb3921de8a00c82f173a509359c32
Laurence Cortez <laurencecortez26.lc@gmail.com>
2025-09-28 13:39:11 +0800

    delete the CreateExpenseForm and Dialog to reuse the ExpenseForm component

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -4,29 +4,30 @@ import AddIcon from '@mui/icons-material/Add';
 import Box from '@mui/material/Box';
 import ExpenseList from '@/components/molecules/ExpensesList';
 import { AppContext } from '@/context/AppContext';
-import AddExpenseDialog from '@/components/organisms/AddExpenseDialog';
 import ExpenseDialog from '@/components/organisms/ExpenseDialog';
 
 export default function Home() {
-  const { expenses, fetchExpenses, expenseFormOpen, setExpenseFormOpen } =
+  const { expenses, fetchExpenses, setExpenseFormOpen, setFormAction } =
     use(AppContext);
-
-  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     fetchExpenses();
   }, []);
 
+  const addExpenseClickHandler = () => {
+    setExpenseFormOpen(true);
+    setFormAction('create');
+  };
+
   return (
     <>
-      <AddExpenseDialog open={open} handleClose={() => setOpen(false)} />
       <ExpenseDialog />
       <Box sx={{ mt: 5, px: 2, maxWidth: '100%' }}>
         <Stack direction="row" justifyContent="end" sx={{ mb: 2 }}>
           <Button
             variant="contained"
             startIcon={<AddIcon />}
-            onClick={() => setOpen(true)}
+            onClick={addExpenseClickHandler}
           >
             Add Expense
           </Button>
