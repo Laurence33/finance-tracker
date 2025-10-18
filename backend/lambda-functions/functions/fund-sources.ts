c3c17696024828da28dc4b1bf2974b31a6c5ccf5
Laurence Cortez <laurencecortez26.lc@gmail.com>
2025-10-18 13:58:52 +0800

    new lambda basic setup for fund-sources

---
+++ backend/lambda-functions/functions/fund-sources.ts
@@ -0,0 +1,46 @@
+import middy from '@middy/core';
+import cors from '@middy/http-cors';
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import {
+    HttpMethod,
+    HttpStatus,
+    createBadRequestResponse,
+    createSuccessResponse,
+    createServerErrorResponse,
+} from 'ft-common-layer';
+
+/**
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ *
+ */
+
+const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    try {
+        // const expensesService = new ExpensesService();
+        switch (event.httpMethod) {
+            case HttpMethod.GET:
+                // return await expensesService.getExpenses(month);
+                return createSuccessResponse(HttpStatus.OK, "Hi, I'm the new lambda for fund sources!");
+            default:
+                return createBadRequestResponse(HttpStatus.BAD_REQUEST, 'Invalid request method.');
+        }
+    } catch (err) {
+        console.log(err);
+        return createServerErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, 'Unexpected error occurred.');
+    }
+};
+
+export const lambdaHandler = middy()
+    .use(
+        cors({
+            headers: 'Content-Type',
+            methods: 'GET, OPTIONS',
+            origins: ['http://localhost:8001'], // TODO: maybe put this to the env variables
+        }),
+    )
+    .handler(handler);
