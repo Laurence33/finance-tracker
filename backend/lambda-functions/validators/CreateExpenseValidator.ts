290c8a93e4f1d3b076bbdc2e9e7f5bfabba1597d
Laurence Cortez <laurencecortez26.lc@gmail.com>
2025-10-18 15:52:54 +0800

    refactor: format CreateExpenseValidator for consistency

--- backend/lambda-functions/validators/CreateExpenseValidator.ts
+++ backend/lambda-functions/validators/CreateExpenseValidator.ts
@@ -1,14 +1,13 @@
-/* eslint-disable prettier/prettier */
 import * as z from 'zod/v4';
 
 export const CreateExpenseValidator = z.object({
-  timestamp: z
-    .string()
-    .transform((value) => new Date(value))
-    .refine((date) => !isNaN(date.getTime()), {
-      message: 'Invalid date format',
-    })
-    .transform((date) => date.toISOString().replace('Z', '').replace('T', ' ')),
-  amount: z.number().positive(),
-  fundSource: z.string(), // TODO: check against a list of allowed fund sources later
+    timestamp: z
+        .string()
+        .transform((value) => new Date(value))
+        .refine((date) => !isNaN(date.getTime()), {
+            message: 'Invalid date format',
+        })
+        .transform((date) => date.toISOString().replace('Z', '').replace('T', ' ')),
+    amount: z.number().positive(),
+    fundSource: z.string(), // TODO: check against a list of allowed fund sources later
 });
